5cee3f4febccd6d751e35c43d24f17b66cda45b3
Никита Лясковский <124239663+niki6ka@users.noreply.github.com>
2023-03-11 15:39:54 +0300

    Add files via upload

---
+++ 64.cs
@@ -0,0 +1,16 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Please enter the number: ");
+int Second = Convert.ToInt32(Console.ReadLine());
+int First = 1;
+Console.WriteLine(IntegerNumber(Second, First));
+
+int IntegerNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write(IntegerNumber(n, m + 1) + " ");
+    return m;
+}
